6be7f0e6fd1567796607c7792bae7f182c3b27fc
tom0334 <tfriederich@live.nl>
2019-09-18 12:32:05 +0200

    Communicate with webserver

--- Frontend/App.vue
+++ Frontend/App.vue
@@ -5,9 +5,23 @@
 </template>
 
 <script lang="ts">
+const axios = require("axios");
 import Vue from "vue";
 
 export default Vue.extend({
+  created: function() {
+    axios
+      .get("http://localhost:3000/")
+
+      .then(function(response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error);
+      });
+  },
   data() {
     return {
       bundler: "Parcel"
