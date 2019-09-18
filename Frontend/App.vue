717a2fc274c36dca172f60889e50b0d18f4a1b2b
tom0334 <tfriederich@live.nl>
2019-09-18 12:06:40 +0200

    Init vue project

---
+++ Frontend/App.vue
@@ -0,0 +1,23 @@
+// App.vue
+
+<template lang="pug">
+  .container Hello {{bundler}}
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      bundler: "Parcel"
+    };
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.container {
+  color: red;
+}
+</style>
